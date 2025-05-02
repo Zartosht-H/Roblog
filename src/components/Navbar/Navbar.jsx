704f881161a3521d56c853fc9b67b19d4605ffb2
Hassan <xartosht.h@live.com>
2025-05-02 09:37:36 +0330

    Everything looks fine Here

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import style from "./navbar.module.css"
+
+function Navbar(){
+ return (
+  <div className={style.navbar}>
+   Go home baby This is my House !
+  </div>
+  );
+}
+
+export default Navbar;
