8b3f7cc34ea7fa73896344621ca72b15a417fd02
Hassan <Zartosht.h@live.com>
2025-05-04 02:43:00 +0330

    create Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import style from "./navbar.module.css"
+import logo from "../../assets/logo.svg"
 
-function Navbar(){
+function Navbar(props){
  return (
-  <div className={style.navbar}>
-   Go home baby This is my House !
-  </div>
+  <nav className={style.navbar}>
+   <div className="container">
+    <img src={logo} alt="Page Logo" />
+    <h1>{props.title}</h1>
+    <ul>
+     <li><a href="#">مقاله‌ها</a></li>
+     <li><a href="#">مقاله جدید</a></li>
+     <li><a href="#">دسته‌بندی</a></li>
+     <li><a href="#">شماوما</a></li>
+    </ul>
+   </div>
+  </nav>
   );
 }
 
