9ae5c6728c14de23edd9b80f9e1ac49c70673fc4
Hassan <hassan.doodiyan@gmail.com>
2025-05-10 17:22:51 +0330

    Create local database, get data from local api and show it in the blogCard component and create a responsive user frindly ui

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,19 +1,17 @@
-import logo from "../../assets/logo.svg"
+import style from "./blogCard.module.css";
 
 
-function Card(props){
- return (
-  <div className="card" style={{width: "18rem"}}>
-   <img src={logo} className="card-img-top" alt="..." />
-  <div className="card-body">
-   <h5 className="card-title">Card title</h5>
-   <p className="card-text">
-    Some quick example text to build on the card title and make up the bulk of the card's content.
-   </p>
-   <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
- </div>
-);
+function BlogCard(props) {
+  return (
+    <div className={`card ${props.class}`} style={{ width: "18rem" }}>
+      <img src={props.blog.imageUrl} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{props.blog.title}</h5>
+        <p className="card-text">{props.blog.discription}</p>
+        <a href="#" className="btn btn-primary">{props.blog.ttr}</a>
+      </div>
+    </div >
+  );
 }
 
-export default Card;
+export default BlogCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import Navbar from "../../components/Navbar/Navbar";
 import BlogCard from "../../components/BlogCard/BlogCard";
+import style from "./home.module.css";
 
 function Home() {
 
- return (
-  <div>
-   <div>
-    <Navbar title="roBlog" />
-    <div className="container">
-     <BlogCard />
-    </div>
-   </div>
-  </div>
- );
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/blogs')
+            .then(function (response) {
+                setBlogs(response.data.data);
+            })
+            .catch(function (error) {
+                console.error("Error details:", error);
+                if (error.response) {
+                    console.error("Status:", error.response.status);
+                    console.error("Data:", error.response.data);
+                }
+            });
+    }, []);
+
+    return (
+        <div>
+            <Navbar title="roBlog" />
+            <div className="container">
+                <h1>آخرین مقالات</h1>
+                <div className={style.blogs}>
+                    {blogs.length > 0 ? (
+                        blogs.map((blog, index, arr) => (
+                            <BlogCard
+                                key={blog.id}
+                                blog={blog}
+                                class={blog.id == arr.length ? "hidden" : ""}
+                            />
+                        ))
+                    ) : (
+                        <p>در حال بارگذاری مقالات...</p>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
