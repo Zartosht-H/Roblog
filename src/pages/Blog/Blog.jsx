a6ab5dba5e3f7554c750954b80d2e031e8787eb3
Hassan <hassan.doodiyan@gmail.com>
2025-05-15 15:15:42 +0330

    Edit the blog page style

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import style from "./blog.module.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
@@ -8,7 +9,7 @@ function Blog() {
   const [blog, setBlog] = useState({});
   const blogId = useParams().id;
   useEffect(() => {
-    axios.get("http://localhost:3000/blogs/?id="+blogId).then((response) => {
+    axios.get("http://localhost:3000/blogs/?id=" + blogId).then((response) => {
       setBlog(response.data[0]);
     });
   }, []);
@@ -17,14 +18,13 @@ function Blog() {
       <Navbar title="roBlog" />
       <div className="container">
         <h1>{blog.title}</h1>
-        <p className="lead">{blog.discription}</p>
-        <ul className="list-group list-group-horizontal">
-          <li className="list-group-item">{blog.date}</li>
-          <li className="list-group-item">{blog.author}</li>
+        <ul className="list-group list-group-horizontal-sm">
+          <li className="list-group-item">تاریخ: {blog.date}</li>
+          <li className="list-group-item">نویسنده: {blog.author}</li>
           <li className="list-group-item">زمان خواندن: {blog.ttr} دقیقه</li>
         </ul>
-        <img src={blog.imageUrl} alt="Blog Image" className="img-fluid" />
-        <p>{blog.blogText}</p>
+        <p className={`lead ${style.discription}`}>{blog.discription}</p>
+        <p className={style.content}><img src={blog.imageUrl} alt="Blog Image" className="img-fluid" />{blog.blogText}</p>
       </div>
       <Footer />
     </>
